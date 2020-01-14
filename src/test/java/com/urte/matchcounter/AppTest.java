9dcc72553a5cec1e5993718f8825353c8c2549fa
Urte <urte.rimsaite@gmail.com>
2020-01-14 11:35:58 +0200

    Add input validation tests

--- src/test/java/com/urte/matchcounter/AppTest.java
+++ src/test/java/com/urte/matchcounter/AppTest.java
@@ -1,20 +1,34 @@
 package com.urte.matchcounter;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void exitsIfArgsLengthIncorrect() {
+        exit.expectSystemExitWithStatus(1);
+        exit.checkAssertionAfterwards(() -> assertEquals("Please enter two numbers\n",
+                systemOutRule.getLogWithNormalizedLineSeparator()));
+        App.main(new String[]{"1"});
+    }
+
+    @Test
+    public void exitsIfCannotParseNumbers() {
+        exit.expectSystemExitWithStatus(1);
+        exit.checkAssertionAfterwards(() -> assertEquals("Please enter two numbers\n",
+                systemOutRule.getLogWithNormalizedLineSeparator()));
+        App.main(new String[]{"A", "B"});
     }
 }
