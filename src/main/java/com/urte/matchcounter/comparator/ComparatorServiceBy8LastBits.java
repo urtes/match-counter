f64e55af2298fdc1c3021428d76244fd2518543a
Urte <urte.rimsaite@gmail.com>
2020-01-14 10:29:59 +0200

    Simplify comparison logic

--- src/main/java/com/urte/matchcounter/comparator/ComparatorServiceBy8LastBits.java
+++ src/main/java/com/urte/matchcounter/comparator/ComparatorServiceBy8LastBits.java
@@ -11,32 +11,13 @@ public class ComparatorServiceBy8LastBits implements ComparatorService {
 
         String a = numberA.toString(2);
         String b = numberB.toString(2);
-        int aLength = a.length();
-        int bLength = b.length();
 
-        if (aLength <= comparisonLength && bLength <= comparisonLength) {
-            return a.equals(b);
-        } else if (aLength > comparisonLength && bLength > comparisonLength) {
+        // It is accepted, that if either of the resulting binary strings is shorter than comparisonLength (8),
+        // strings cannot be compared and result is false.
+        try {
             return b.endsWith(a.substring(a.length() - comparisonLength));
-        } else  if (aLength > comparisonLength) {
-            if (endingFits(a, b)) {
-                return restAreZeroes(a, b.length());
-            }
-            return false;
-        } else {
-            if (endingFits(b, a)) {
-                return restAreZeroes(b, a.length());
-            }
+        } catch (StringIndexOutOfBoundsException e) {
             return false;
         }
-    }
-
-    private boolean endingFits(String longer, String shorter) {
-        return longer.endsWith(shorter);
-    }
-
-    private boolean restAreZeroes (String stringToCheck, int endingIndex) {
-        String substringToCheck = stringToCheck.substring(stringToCheck.length() - comparisonLength, endingIndex);
-        return Integer.parseInt(substringToCheck) == 0;
     }
 }
